4a5f439ea894e94582a16c155aa7b6665587d124
nguyenthitomyagu-lab <nguyenthitomy.agu@gmail.com>
2025-09-30 14:06:01 +0700

    Update vite.config.ts

--- .github/workflows/vite.config.ts
+++ .github/workflows/vite.config.ts
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/check_AI/',   // rất quan trọng khi chạy GitHub Pages mặc định
+  base: '/check_AI/',
 })
